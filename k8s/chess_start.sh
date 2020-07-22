b33b323278bd16287946479f8dd818eab43d5b52
Donát Udvari <udvarid@hotmail.com>
2020-07-22 10:28:52 +0200

    k8s files

---
+++ k8s/chess_start.sh
@@ -0,0 +1,5 @@
+kubectl create -f chess_backend.yaml
+kubectl create -f chess_backend_s.yaml
+kubectl create -f chess_frontend.yaml
+kubectl create -f chess_frontend_s.yaml
+kubectl create -f chess_ingress.yaml
