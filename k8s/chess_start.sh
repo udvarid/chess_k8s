3e0fe0da6446210eecaf9b580aaf4a3a98f76537
Donát Udvari <udvarid@hotmail.com>
2020-07-23 09:24:19 +0200

    Turn on Ingress to reach backend

--- k8s/chess_start.sh
+++ k8s/chess_start.sh
@@ -1,5 +1,19 @@
-kubectl create -f chess_backend.yaml
-kubectl create -f chess_backend_s.yaml
-kubectl create -f chess_frontend.yaml
-kubectl create -f chess_frontend_s.yaml
-kubectl create -f chess_ingress.yaml
+echo delete previous deployments
+kubectl delete -n chess deployment frontend
+kubectl delete -n chess deployment backend
+kubectl delete -n chess service frontend
+kubectl delete -n chess service backend
+kubectl delete -n chess ingress chess-ingress
+
+echo delete namespace
+kubectl delete namespace chess
+
+echo create namespace
+kubectl create namespace chess
+
+echo create new deployments
+kubectl create -f chess_backend.yaml -n chess
+kubectl create -f chess_backend_s.yaml -n chess
+kubectl create -f chess_frontend.yaml -n chess
+kubectl create -f chess_frontend_s.yaml -n chess
+kubectl create -f chess_ingress.yaml -n chess
